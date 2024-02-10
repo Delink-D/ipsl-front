4ad32999246ca7556393f3f128f7031b05421bad
Delink <derick@health-e-net.org>
2024-02-10 06:31:51 +0300

    add side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { AppMaterialModule } from './material/material.module';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [],
+  imports: [
+    HttpClientModule,
+    RouterOutlet,
+    AppMaterialModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile = true;
+  isCollapsed = true;
+
+  constructor(
+    private mediaObserver: BreakpointObserver
+  ) { }
+
+  ngOnInit() {
+    this.mediaObserver.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if (screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  /**
+   * Toggle the humbugger menu 
+   */
+  toggleMenu() {
+    if (this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    } else {
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
