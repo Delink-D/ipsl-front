2e18e5cad1ce6ab94f2a52dfc80f3b4dd8484b2c
Delink <derick@health-e-net.org>
2024-02-10 18:39:05 +0300

    add the rest of the user form fields

--- src/app/users/components/add-user/add-user.component.ts
+++ src/app/users/components/add-user/add-user.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { AppMaterialModule } from '../../../material/material.module';
 
 import { User } from '../../../models/user';
 
-import { UserService } from '../../../services/user.service';
 import { ValidationService } from '../../../validations/validation.service';
 import { ValidationMessagesComponent } from '../../../validations/validation-message.component';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [CommonModule, AppMaterialModule, FormsModule, ReactiveFormsModule,ValidationMessagesComponent],
+  imports: [CommonModule, AppMaterialModule, FormsModule, ReactiveFormsModule, ValidationMessagesComponent],
   providers: [],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
@@ -32,12 +30,17 @@ export class AddUserComponent {
       email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(255)]),
       phone: new FormControl('', [Validators.required]),
       website: new FormControl('', [ValidationService.websiteValidator, Validators.minLength(5), Validators.maxLength(255)]),
+
       addressStreet: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
       addressSuite: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
       addressCity: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
       addressZipCode: new FormControl('', [Validators.minLength(3), Validators.maxLength(255)]),
       addressGeoLat: new FormControl('', [Validators.minLength(3), Validators.maxLength(255)]),
       addressGeoLng: new FormControl('', [Validators.minLength(3), Validators.maxLength(255)]),
+
+      companyName: new FormControl('', [Validators.minLength(3), Validators.maxLength(50)]),
+      companyCatchPhrase: new FormControl('', [Validators.minLength(3), Validators.maxLength(255)]),
+      companyBs: new FormControl('', [Validators.minLength(3), Validators.maxLength(255)])
     });
   }
 
