a2c22964c62317e2e1c5abf17ed82e2d5a16f030
Delink <derick@health-e-net.org>
2024-02-10 14:04:52 +0300

    display users in a table

--- src/app/users/components/users-list/user-list.component.ts
+++ src/app/users/components/users-list/user-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSort } from '@angular/material/sort';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { User } from '../../../models/user';
 
 import { UserService } from '../../../services/user.service';
+import { AppMaterialModule } from '../../../material/material.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppMaterialModule],
   providers: [],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
@@ -17,7 +22,14 @@ import { UserService } from '../../../services/user.service';
 export class UserListComponent implements OnInit {
   users!: User[];
 
+  displayedColumns: string[] = ['image', 'name', 'email', 'company_name'];
+  dataSource!: MatTableDataSource<User>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(
+    private router: Router,
     private snackBar: MatSnackBar,
     private userService: UserService
   ) { }
@@ -31,8 +43,34 @@ export class UserListComponent implements OnInit {
    */
   getUsers() {
     this.userService.getAllUsers().subscribe(
-      (response: User[]) => this.users = response),
+      (response: User[]) => {
+        this.users = response;
+        this.dataSource = new MatTableDataSource(this.users);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }),
       (error: any) => { console.log(error); this.snackBar.open('Error getting list of users', 'Error', { duration: 6000 }) },
       () => console.log('Completed fetching all the users')
+  }
+
+  /**
+   * Filter the users table 
+   * @param event filter event
+   */
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  /**
+   * View more details of the selected user
+   * @param {number} userId 
+   */
+  viewUser(userId: number) {
+    this.router.navigate(['/users/' + userId]);
   }
 }
