7ae953d5f727455a24670247d5d185ed3286e9be
Delink <derick@health-e-net.org>
2024-02-10 08:21:39 +0300

    update the user model with the fields list

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,34 @@
 
 export class User {
+  id!: number;
+  name!: string;
+  username!: string;
+  email!: string;
+  address!: UserAddress;
+  phone!: string;
+  website!: string;
+  company!: UserCompany;
 
   constructor(values: Object = {}) {
     (<any>Object).assign(this, values);
   }
+}
+
+export interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: LocationCoordinates;
+}
+
+export interface LocationCoordinates {
+  lat: string;
+  lng: string;
+}
+
+export interface UserCompany {
+  name: string
+  catchPhrase: string
+  bs: string
 }
