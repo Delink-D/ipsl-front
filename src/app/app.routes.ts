bfff22303c5e67ba5443aec7f565f2e8f139b3e7
Delink <derick@health-e-net.org>
2024-02-11 21:25:47 +0300

    add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { ValidationService } from './validations/validation.service';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+  },
+  {
     path: 'users',
     providers: [UserService, ValidationService],
     loadChildren: () => import('./users/users.routes').then(routes => routes.UsersRoutes)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  providers: [],
+  template: `<h1>Welcome to user management app.</h1>`
+})
+export class HomeComponent {
+
+}
