74796388b5f9a1374298800abe14c06dec0774c3
Delink <derick@health-e-net.org>
2024-02-12 11:31:37 +0300

    update service unit test

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,28 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
-import { of } from 'rxjs';
 import { User } from '../models/user';
+import { TestBed } from '@angular/core/testing';
+import { users } from '../mock-data/users';
 
 describe('User Service', () => {
   let service: UserService;
-  let http: HttpClient;
   const API_ENDPOINT = 'https://jsonplaceholder.typicode.com';
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
-    service = new UserService(http);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers:[UserService]
+    });
+    service = TestBed.inject(UserService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('Should create the service', () => {
     expect(service).toBeTruthy();
   });
 
-  // it('Should test getAllUsers', () => {
-  //   const res: User[] = [];
-    
-  //   // jasmine.isSpy(http, 'get').mockReturnValue(of(res));
-  //   service.getAllUsers();
-  //   expect(http.get).toHaveBeenCalledTimes(1);
-  //   expect(http.get).toHaveBeenCalledWith(API_ENDPOINT);
-  //   // expect(http.get).tOhAVE;
-  // });
+  it('Should test getAllUsers', () => {
+    service.getAllUsers().subscribe((users: User[]) => {
+      expect(users).toBeTruthy();
+      expect(users.length).toBe(5);
+    });
+    const mockReq = testingController.expectOne(`${API_ENDPOINT}/users`);
+    expect(mockReq.request.method).toEqual('GET');
+    mockReq.flush(Object.values(users));
+  });
+
+  it('Should test getSingleUser', () => {
+    service.getSingleUser(2).subscribe((user: User) => {
+      console.log(user.name)
+      expect(user).toBeTruthy();
+      expect(user.name).toBe('Ervin Howell');
+    });
+    const mockReq = testingController.expectOne(`${API_ENDPOINT}/users/${2}`);
+    expect(mockReq.request.method).toEqual('GET');
+    mockReq.flush(users[1]);
+  });
 });
