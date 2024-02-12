b4f51ef4a495267ec98f876099ebf3b0d4e60183
Delink <derick@health-e-net.org>
2024-02-12 12:00:16 +0300

    test test cases

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -12,7 +12,7 @@ describe('User Service', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers:[UserService]
+      providers: [UserService]
     });
     service = TestBed.inject(UserService);
     testingController = TestBed.inject(HttpTestingController);
@@ -34,12 +34,68 @@ describe('User Service', () => {
 
   it('Should test getSingleUser', () => {
     service.getSingleUser(2).subscribe((user: User) => {
-      console.log(user.name)
       expect(user).toBeTruthy();
       expect(user.name).toBe('Ervin Howell');
     });
     const mockReq = testingController.expectOne(`${API_ENDPOINT}/users/${2}`);
     expect(mockReq.request.method).toEqual('GET');
     mockReq.flush(users[1]);
+  });
+
+  it('Should test updateUser', () => {
+    const user: User = {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "image": 'https://image.com/image-1.jpg',
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    };
+
+    service.updateUser(user).subscribe((user: User) => {
+      expect(user).toBeTruthy();
+      expect(user.id).toBe(4);
+      expect(user.name).toBe('Patricia Lebsack');
+      expect(user.image).toBe('https://image.com/image-1.jpg');
+      expect(user.company.name).toBe('');
+    });
+    const mockReq = testingController.expectOne(`${API_ENDPOINT}/users/${4}`);
+    expect(mockReq.request.method).toEqual('PUT');
+    let updatedUser: User = <User>users[3]
+    updatedUser.company.name = '';
+    updatedUser.image = 'https://image.com/image-1.jpg';
+    mockReq.flush(updatedUser);
+  });
+
+  it('Should test deleteUser', () => {
+    service.deleteUser(2).subscribe((res: void) => {
+      expect(res).toBeTruthy();
+      expect(res).toEqual(Object());
+    });
+    const mockReq = testingController.expectOne(`${API_ENDPOINT}/users/${2}`);
+    expect(mockReq.request.method).toEqual('DELETE');
+    const deletedList = users.filter(user => user.id !== 2);
+    expect(deletedList.length).toBe(4);
+    mockReq.flush({});
+  });
+
+  afterEach(() => {
+    testingController.verify();
   });
 });
